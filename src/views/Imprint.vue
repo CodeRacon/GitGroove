7f280080aa33f5c2ee4f8595bdc77a223a44c8e5
CodeRacon <michael.buschmann.86@gmail.com>
2024-11-24 03:56:08 +0100

    feat(view): text adjustments

--- src/views/Imprint.vue
+++ src/views/Imprint.vue
@@ -6,43 +6,50 @@
     <div class="content">
       <p>
         I'm Micha, frontend developer, music enthusiast... and interested in a lot of things, to say
-        the least. Sound generation is on of it and has interested me all my life, in one way or
-        another. Of course, this also includes <i>synthetic</i> sound generation.
+        the least.
+        <br />
+        Sound generation is one of those, analogue or <i>synthetic</i> - it has interested me all my
+        life, in one way or another.
       </p>
 
       <p>
         I came across <a href="https://eirikbrandal.com/" target="blank">Eirik Brandal</a> by
-        chance, which left a lasting impression on me:
+        chance, which left quite a lasting impression on me.
         <br />
-        Eirik is man, an <i>artist</i> first and foremost, who creates fascinating, living
-        sound-sculptures with electronics, perseverance, precision, a steady hand and lots of
-        creativity. He was instrumental in turning my initial idea into a finished project. Thank
-        you Eirik!
+        Eirik is an norwegian artist and composer who creates fascinating, living sound-sculptures.
+        Electronic components, perseverance, precision, a steady hand and lots of creativity. That
+        is all he needs to craft something true magic.
       </p>
 
       <p>
         But since I don't know much about electronics <i>yet</i>, I had to come up with something I
         can do: A web-app!
+        <br />
+        Eiriks commitment to bring his pieces of light, sound and magic to life were instrumental in
+        turning my initial idea into a finished project.
       </p>
       <p>
-        <b
-          >GitGroove is intended to be a tool for everyone to craft and develop their own personal
-          sound-sculpture and their personal sonic narrative.</b
-        >
+        GitGroove is intended to be a tool and toy for everyone to craft and develop their own
+        personal sound-sculpture.
       </p>
+      <p>So without further ado:<span> Go make some noise ...I mean, literally!</span></p>
+      <br />
+
+      <hr />
 
       <h3>Project Essence</h3>
       <p>
-        GitGroove combines software development and music in an experimental, playful way. Every
-        contribution becomes a musical element, where Git metrics are mapped to musical components.
-        The result is a harmonious carpet of sound that reflects the development of your projects in
-        an entirely new dimension.
+        GitGroove combines software development and music in an experimental, playful way.
+        <br />
+        Every contribution becomes a musical element, where Git metrics are mapped to musical
+        components. The result is a harmonious carpet of sound that reflects the development of your
+        projects in an entirely new dimension.
       </p>
 
       <h3>Data Usage Notice</h3>
       <p>
         GitGroove only uses publicly available GitHub contribution data to create its sonic
-        patterns. We don't collect or store any private information.
+        patterns. I don't collect or store any private information.
       </p>
 
       <h3>Contact</h3>
@@ -50,23 +57,18 @@
         Do you have questions, concerns, praise, criticism, suggestions for improvement or would
         like to talk about something specific?
         <br />
+        <br />
         Feel free to reach out via
         <a href="mailto:hey@michael-buschmann.dev">hey@michael-buschmann.dev</a> or check one of the
         links below:
       </p>
-      <ul>
-        <li>
-          <a href="www.michael-buschmann.dev" target="blank">www.michael-buschmann.dev</a>
-        </li>
-        <li>
-          <a href="https://github.com/CodeRacon" target="blank">GitHub Profile</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/michael-buschmann-front-end-dev/" target="blank"
-            >LinkedIn Profile</a
-          >
-        </li>
-      </ul>
+      <div class="links">
+        <a href="www.michael-buschmann.dev" target="blank">www.michael-buschmann.dev</a>
+        <a href="https://github.com/CodeRacon" target="blank">GitHub </a>
+        <a href="https://www.linkedin.com/in/michael-buschmann-front-end-dev/" target="blank"
+          >LinkedIn
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -83,6 +85,11 @@
   line-height: 1.6;
 }
 
+.links {
+  display: flex;
+  flex-direction: column;
+}
+
 h3 {
   margin-top: 2rem;
   color: #4caf50;
@@ -93,7 +100,16 @@ p {
   color: #cad9cb;
 }
 
+a {
+  text-decoration-line: underline;
+}
+
 a:hover {
   color: #7dede0;
+}
+
+span {
+  font-weight: bold;
+  color: white;
 }
 </style>
